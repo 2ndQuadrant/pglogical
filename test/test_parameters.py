f88a2bcb755c643644bcef59a9764382c69bc48c
Craig Ringer <craig@2ndquadrant.com>
2015-10-05 12:56:54 +0800

    Implement support for testing walsender connections
    To run the tests using a walsender connection you must set the
    environment variable PGLOGICALTEST_USEWALSENDER=1 .
    
    These tests don't pass yet due to bugs in the output plugin.
    
    Walsender based tests require a patched psycopg2. Currently the patch
    is pending merge into the psycopg2 tree and is available at
    https://github.com/psycopg/psycopg2/pull/322
    
    The API used by psycopg2's replication support may change before it's
    merged, so this code might break in future, but that won't affect
    the SQL test support.

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -8,8 +8,13 @@ class ParametersTest(PGLogicalOutputTest):
 
     def setUp(self):
         PGLogicalOutputTest.setUp(self)
+        self.cur.execute("DROP TABLE IF EXISTS blah;")
+        self.cur.execute("CREATE TABLE blah(id integer);")
+        self.conn.commit()
 
     def tearDown(self):
+        self.cur.execute("DROP TABLE blah;")
+        self.conn.commit()
         PGLogicalOutputTest.tearDown(self)
 
     def test_protoversion(self):
@@ -38,15 +43,18 @@ class ParametersTest(PGLogicalOutputTest):
             list(self.get_changes({'max_proto_version': 'borkbork'}))
 
     def test_unknown_params(self):
-        # Should all get ignored
-        list(self.get_changes({'unknown_required_parameter': 'unknown'}))
-        list(self.get_changes({'unknown.some_param': 'unknown'}))
-        list(self.get_changes({'Unknown.some_param': 'unknown'}))
-        list(self.get_changes({'Unknown.Some_param': 'unknown'}))
+        # Should get ignored
+        self.do_dummy_tx()
+        self.get_startup_msg(self.get_changes({'unknown_parameter': 'unknown'}))
+
+    def test_unknown_params(self):
+        # Should get ignored
+        self.do_dummy_tx()
+        self.get_startup_msg(self.get_changes({'unknown.some_param': 'unknown'}))
 
     def test_encoding_missing(self):
         # Should be ignored, server should send reply params
-        list(self.get_changes({'expected_encoding': None}))
+        messages = self.get_changes({'expected_encoding': None})
 
     def test_encoding_bogus(self):
         with self.assertRaises(psycopg2.DatabaseError):
@@ -56,6 +64,16 @@ class ParametersTest(PGLogicalOutputTest):
         with self.assertRaises(psycopg2.DatabaseError):
             list(self.get_changes({'expected_encoding': 'LATIN-1'}))
 
+    def do_dummy_tx(self):
+        """force a dummy tx so there's something to decode"""
+        self.cur.execute("INSERT INTO blah(id) VALUES (1)")
+        self.conn.commit()
+
+    def get_startup_msg(self, messages):
+        """Read and return the startup message"""
+        m = messages.next()
+        self.assertEqual(m.mesage_type, 'S')
+        return m
 
 if __name__ == '__main__':
     unittest.main()
