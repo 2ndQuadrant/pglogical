fae35054fda5b1b86d3fa91fd4426955a18a8a3d
Petr Jelinek <pjmodos@pjmodos.net>
2015-10-21 01:03:29 +0200

    Remove reference to nonexisting function

--- pglogical.h
+++ pglogical.h
@@ -35,6 +35,4 @@ extern List *textarray_to_list(ArrayType *textarray);
 
 void pglogical_execute_sql_command(char *cmdstr, char *role, bool isTopLevel);
 
-void pglogical_commandfilter_init(void);
-
 #endif /* PGLOGICAL_H */
