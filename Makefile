b240a48b22e68b743a13a96a8a3dae272df9f2bc
Pallavi Sontakke <pallavi.sontakke@2ndQuadrant.com>
2015-12-24 09:53:01 +0530

    Add COPY test
    Fixes #50

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ SCRIPTS_built = pglogical_create_subscriber
 PG_CPPFLAGS = -I$(libpq_srcdir)
 SHLIB_LINK = $(libpq)
 
-REGRESS = preseed infofuncs init_fail init preseed_check basic extended toasted replication_set add_table matview bidirectional primary_key foreign_key functions drop
+REGRESS = preseed infofuncs init_fail init preseed_check basic extended toasted replication_set add_table matview bidirectional primary_key foreign_key functions copy drop
 
 EXTRA_CLEAN += pglogical.control
 
---
+++ sql/copy.sql
@@ -0,0 +1,46 @@
+--test COPY
+\c regression
+
+SELECT pglogical.replicate_ddl_command($$
+     CREATE TABLE public.x (
+	a serial primary key,
+	b int,
+	c text not null default 'stuff',
+	d text,
+	e text
+     );
+$$);
+
+SELECT * FROM pglogical.replication_set_add_table('default', 'x');
+
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+
+COPY x (a, b, c, d, e) from stdin;
+9999	\N	\\N	\NN	\N
+10000	21	31	41	51
+\.
+
+COPY x (b, d) from stdin;
+1	test_1
+\.
+
+COPY x (b, d) from stdin;
+2	test_2
+3	test_3
+4	test_4
+5	test_5
+\.
+
+COPY x (a, b, c, d, e) from stdin;
+10001	22	32	42	52
+10002	23	33	43	53
+10003	24	34	44	54
+10004	25	35	45	55
+10005	26	36	46	56
+\.
+
+SELECT * FROM x ORDER BY a;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+
+\c postgres
+SELECT * FROM x ORDER BY a;
