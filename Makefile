2ndQuadrant/pglogical

commit b644896b6f6d2bc719d66bf3aacc0abc501c6453

Add requires line for 9.4 control file

Makefile
@@ -44,6 +44,7 @@ SHLIB_LINK += $(libpq)
 
 PGVER := $(shell $(PG_CONFIG) --version | sed 's/[^0-9\.]//g' | awk -F . '{ print $$1$$2 }')
 
+requires=""
 ifeq ($(PGVER),94)
 PG_CPPFLAGS += $(addprefix -I,$(realpath $(srcdir)/compat94))
 OBJS += $(srcdir)/compat94/pglogical_compat.o
@@ -54,6 +55,7 @@ REGRESS = preseed infofuncs init_fail init preseed_check basic extended \
 REGRESS += --dbname=regression
 SCRIPTS_built += pglogical_dump/pglogical_dump
 SCRIPTS += pglogical_dump/pglogical_dump
+requires="requires=pglogical_origin"
 endif
 ifeq ($(PGVER),95)
 PG_CPPFLAGS += $(addprefix -I,$(realpath $(srcdir)/compat95))
@@ -115,6 +117,7 @@ clean: pglogical-dump-clean
 
 pglogical.control: pglogical.control.in pglogical.h
 	sed 's/__PGLOGICAL_VERSION__/$(pglogical_version)/' $(realpath $(srcdir)/pglogical.control.in) > pglogical.control
+	sed 's/__REQUIRES__/$(requires)/' $(realpath $(srcdir)/pglogical.control.in) > pglogical.control
 
 all: pglogical.control
 

pglogical.control.in
@@ -3,4 +3,5 @@ comment = 'PostgreSQL Logical Replication'
 default_version = '__PGLOGICAL_VERSION__'
 module_pathname = '$libdir/pglogical'
 relocatable = false
+__REQUIRES__
 schema = pglogical

commit c8694f63d0b1c93a2d1d477e80f4817a46e360c1

Fix broken control file generation caused by b644896

Makefile
@@ -44,7 +44,7 @@ SHLIB_LINK += $(libpq)
 
 PGVER := $(shell $(PG_CONFIG) --version | sed 's/[^0-9\.]//g' | awk -F . '{ print $$1$$2 }')
 
-requires=""
+requires=
 ifeq ($(PGVER),94)
 PG_CPPFLAGS += $(addprefix -I,$(realpath $(srcdir)/compat94))
 OBJS += $(srcdir)/compat94/pglogical_compat.o
@@ -116,8 +116,7 @@ pglogical-dump-clean:
 clean: pglogical-dump-clean
 
 pglogical.control: pglogical.control.in pglogical.h
-	sed 's/__PGLOGICAL_VERSION__/$(pglogical_version)/' $(realpath $(srcdir)/pglogical.control.in) > pglogical.control
-	sed 's/__REQUIRES__/$(requires)/' $(realpath $(srcdir)/pglogical.control.in) > pglogical.control
+	sed 's/__PGLOGICAL_VERSION__/$(pglogical_version)/;s/__REQUIRES__/$(requires)/' $(realpath $(srcdir)/pglogical.control.in) > pglogical.control
 
 all: pglogical.control
 