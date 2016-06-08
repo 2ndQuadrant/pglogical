1d5ebbc5dd9d0a07228395e89e3e1412a04fecb1
Petr Jelinek <pjmodos@pjmodos.net>
2016-06-08 16:51:15 +0200

    Debian packaging compat

--- pglogical_create_subscriber.c
+++ pglogical_create_subscriber.c
@@ -22,6 +22,19 @@
 #include <unistd.h>
 
 /* Note the order is important for debian here. */
+#if !defined(pg_attribute_printf)
+
+/* GCC and XLC support format attributes */
+#if defined(__GNUC__) || defined(__IBMC__)
+#define pg_attribute_format_arg(a) __attribute__((format_arg(a)))
+#define pg_attribute_printf(f,a) __attribute__((format(PG_PRINTF_ATTRIBUTE, f, a)))
+#else
+#define pg_attribute_format_arg(a)
+#define pg_attribute_printf(f,a)
+#endif
+
+#endif
+
 #include "libpq-fe.h"
 #include "postgres_fe.h"
 #include "pqexpbuffer.h"
