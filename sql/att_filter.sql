af77ddcb5032ca3176a3420a1f4387510b855afc
Petr Jelinek <pjmodos@pjmodos.net>
2016-10-08 10:55:32 +0200

    Attribute and row filter tests

---
+++ sql/att_filter.sql
@@ -0,0 +1,98 @@
+-- basic builtin datatypes
+SELECT * FROM pglogical_regress_variables()
+\gset
+
+\c :provider_dsn
+CREATE TABLE public.basic_dml (
+	id serial primary key,
+	other integer,
+	data text,
+	something interval
+);
+
+SELECT * FROM pglogical.replication_set_add_table('default', 'basic_dml', att_filter := '{id, data, something}');
+
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+
+\c :subscriber_dsn
+
+CREATE TABLE public.basic_dml (
+	id serial primary key,
+	data text,
+	something interval,
+	subonly integer,
+	subonly_def integer DEFAULT 99
+);
+
+\c :provider_dsn
+
+-- check basic insert replication
+INSERT INTO basic_dml(other, data, something)
+VALUES (5, 'foo', '1 minute'::interval),
+       (4, 'bar', '12 weeks'::interval),
+       (3, 'baz', '2 years 1 hour'::interval),
+       (2, 'qux', '8 months 2 days'::interval),
+       (1, NULL, NULL);
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something FROM basic_dml ORDER BY id;
+
+-- update one row
+\c :provider_dsn
+UPDATE basic_dml SET other = '4', data = NULL, something = '3 days'::interval WHERE id = 4;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something FROM basic_dml ORDER BY id;
+
+-- update multiple rows
+\c :provider_dsn
+UPDATE basic_dml SET other = id, data = data || id::text;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something FROM basic_dml ORDER BY id;
+
+\c :provider_dsn
+UPDATE basic_dml SET other = id, something = something - '10 seconds'::interval WHERE id < 3;
+UPDATE basic_dml SET other = id, something = something + '10 seconds'::interval WHERE id > 3;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something, subonly, subonly_def FROM basic_dml ORDER BY id;
+
+-- delete one row
+\c :provider_dsn
+DELETE FROM basic_dml WHERE id = 2;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something FROM basic_dml ORDER BY id;
+
+-- delete multiple rows
+\c :provider_dsn
+DELETE FROM basic_dml WHERE id < 4;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something FROM basic_dml ORDER BY id;
+
+-- truncate
+\c :provider_dsn
+TRUNCATE basic_dml;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something FROM basic_dml ORDER BY id;
+
+-- copy
+\c :provider_dsn
+\COPY basic_dml FROM STDIN WITH CSV
+9000,1,aaa,1 hour
+9001,2,bbb,2 years
+9002,3,ccc,3 minutes
+9003,4,ddd,4 days
+\.
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, data, something FROM basic_dml ORDER BY id;
+
+\c :provider_dsn
+\set VERBOSITY terse
+SELECT pglogical.replicate_ddl_command($$
+	DROP TABLE public.basic_dml CASCADE;
+$$);
---
+++ sql/row_filter.sql
@@ -0,0 +1,111 @@
+-- row based filtering
+SELECT * FROM pglogical_regress_variables()
+\gset
+
+\c :provider_dsn
+SELECT pglogical.replicate_ddl_command($$
+	CREATE TABLE public.basic_dml (
+		id serial primary key,
+		other integer,
+		data text,
+		something interval
+	);
+$$);
+
+-- we allow volatile functions, it's user's responsibility to not do writes
+SELECT * FROM pglogical.replication_set_add_table('default', 'basic_dml', false, row_filter := 'current_user = data');
+SELECT * FROM pglogical.replication_set_remove_table('default', 'basic_dml');
+-- fail -- subselect
+SELECT * FROM pglogical.replication_set_add_table('default', 'basic_dml', false, row_filter := '(SELECT count(*) FROM pg_class) > 1');
+-- fail -- SELECT
+SELECT * FROM pglogical.replication_set_add_table('default', 'basic_dml', false, row_filter := 'SELECT true');
+-- fail -- nonexisting column
+SELECT * FROM pglogical.replication_set_add_table('default', 'basic_dml', false, row_filter := 'foobar');
+-- fail -- not coercable to bool
+SELECT * FROM pglogical.replication_set_add_table('default', 'basic_dml', false, row_filter := 'data');
+
+-- use this filter for rest of the test
+SELECT * FROM pglogical.replication_set_add_table('default', 'basic_dml', false, row_filter := $rf$id > 1 AND data IS DISTINCT FROM 'baz' AND data IS DISTINCT FROM 'bbb'$rf$);
+
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+
+\c :subscriber_dsn
+
+ALTER TABLE public.basic_dml ADD COLUMN subonly integer;
+ALTER TABLE public.basic_dml ADD COLUMN subonly_def integer DEFAULT 99;
+
+\c :provider_dsn
+
+-- check basic insert replication
+INSERT INTO basic_dml(other, data, something)
+VALUES (5, 'foo', '1 minute'::interval),
+       (4, 'bar', '12 weeks'::interval),
+       (3, 'baz', '2 years 1 hour'::interval),
+       (2, 'qux', '8 months 2 days'::interval),
+       (1, NULL, NULL);
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something FROM basic_dml ORDER BY id;
+
+-- update one row
+\c :provider_dsn
+UPDATE basic_dml SET other = '4', data = NULL, something = '3 days'::interval WHERE id = 4;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something FROM basic_dml ORDER BY id;
+
+-- update multiple rows
+\c :provider_dsn
+UPDATE basic_dml SET other = id, data = data || id::text;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something FROM basic_dml ORDER BY id;
+
+\c :provider_dsn
+UPDATE basic_dml SET other = id, something = something - '10 seconds'::interval WHERE id < 3;
+UPDATE basic_dml SET other = id, something = something + '10 seconds'::interval WHERE id > 3;
+DELETE FROM basic_dml WHERE id = 3;
+INSERT INTO basic_dml VALUES (3, 99, 'bazbaz', '2 years 1 hour'::interval);
+UPDATE basic_dml SET data = 'baz' WHERE id = 3;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something, subonly, subonly_def FROM basic_dml ORDER BY id;
+
+-- delete one row
+\c :provider_dsn
+DELETE FROM basic_dml WHERE id = 2;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something FROM basic_dml ORDER BY id;
+
+-- delete multiple rows
+\c :provider_dsn
+DELETE FROM basic_dml WHERE id < 4;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something FROM basic_dml ORDER BY id;
+
+-- truncate
+\c :provider_dsn
+TRUNCATE basic_dml;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something FROM basic_dml ORDER BY id;
+
+-- copy
+\c :provider_dsn
+\COPY basic_dml FROM STDIN WITH CSV
+9000,1,aaa,1 hour
+9001,2,bbb,2 years
+9002,3,ccc,3 minutes
+9003,4,ddd,4 days
+\.
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+\c :subscriber_dsn
+SELECT id, other, data, something FROM basic_dml ORDER BY id;
+
+\c :provider_dsn
+\set VERBOSITY terse
+SELECT pglogical.replicate_ddl_command($$
+	DROP TABLE public.basic_dml CASCADE;
+$$);
