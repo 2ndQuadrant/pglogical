bf1e8b38ffb45f73f2da601d11227ead43afcaca
Petr Jelinek <pjmodos@pjmodos.net>
2017-04-19 14:03:18 +0200

    Use replay_location instead of flush_location when waiting for replication in tests
    Otherwise changes might not be visible to subsequent selects.

--- t/030_pglogical_daylight_times_switching.pl
+++ t/030_pglogical_daylight_times_switching.pl
@@ -148,7 +148,7 @@ system_or_bail 'psql', '-p', "$PGPORT", '-c', "SELECT sync_kind, sync_subid, syn
 system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE OR REPLACE FUNCTION public.pg_xlog_wait_remote_apply(i_pos pg_lsn, i_pid integer) RETURNS VOID
 AS \$FUNC\$
 BEGIN
-    WHILE EXISTS(SELECT true FROM pg_stat_get_wal_senders() s WHERE s.flush_location < i_pos AND (i_pid = 0 OR s.pid = i_pid)) LOOP
+    WHILE EXISTS(SELECT true FROM pg_stat_get_wal_senders() s WHERE s.replay_location < i_pos AND (i_pid = 0 OR s.pid = i_pid)) LOOP
                 PERFORM pg_sleep(0.01);
         END LOOP;
 END;\$FUNC\$ LANGUAGE plpgsql";
