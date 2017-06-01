2ndQuadrant/pglogical

commit 21cac9095e922cdc4c2c710f5423061bdfa2354d

Postgres-XL: Fixes Bug #120 - pglogical 2.0 no more depends on event triggers. - Modify tests accordingly - correct README typo

expected/init.out
@@ -36,7 +36,7 @@ BEGIN
                 CREATE EXTENSION IF NOT EXISTS pglogical_origin;
         END IF;
 END;$$;
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
 ALTER EXTENSION pglogical UPDATE;
 SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := (SELECT provider_dsn FROM pglogical_regress_variables()) || ' user=super');
  create_node 

sql/init.sql
@@ -47,7 +47,7 @@ BEGIN
         END IF;
 END;$$;
 
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
 ALTER EXTENSION pglogical UPDATE;
 
 SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := (SELECT provider_dsn FROM pglogical_regress_variables()) || ' user=super');

expected/init_fail.out
@@ -22,7 +22,7 @@ BEGIN
 		CREATE EXTENSION IF NOT EXISTS pglogical_origin;
 	END IF;
 END;$$;
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
 ALTER EXTENSION pglogical UPDATE;
 -- fail (local node not existing)
 SELECT * FROM pglogical.create_subscription(

expected/multiple_upstreams.out
@@ -17,7 +17,7 @@ BEGIN
                 CREATE EXTENSION IF NOT EXISTS pglogical_origin;
         END IF;
 END;$$;
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
 ALTER EXTENSION pglogical UPDATE;
 SELECT * FROM pglogical.create_node(node_name := 'test_provider1', dsn := (SELECT provider1_dsn FROM pglogical_regress_variables()) || ' user=super');
  create_node 

expected/multiple_upstreams_1.out
@@ -17,7 +17,7 @@ BEGIN
                 CREATE EXTENSION IF NOT EXISTS pglogical_origin;
         END IF;
 END;$$;
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
 ALTER EXTENSION pglogical UPDATE;
 SELECT * FROM pglogical.create_node(node_name := 'test_provider1', dsn := (SELECT provider1_dsn FROM pglogical_regress_variables()) || ' user=super');
  create_node 

expected/node_origin_cascade.out
@@ -17,7 +17,7 @@ BEGIN
                 CREATE EXTENSION IF NOT EXISTS pglogical_origin;
         END IF;
 END;$$;
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
 ALTER EXTENSION pglogical UPDATE;
 SELECT * FROM pglogical.create_node(node_name := 'test_orig_provider', dsn := (SELECT orig_provider_dsn FROM pglogical_regress_variables()) || ' user=super');
  create_node 

pglogical--2.0.0.sql
@@ -213,9 +213,6 @@ RETURNS boolean STRICT VOLATILE LANGUAGE c AS 'MODULE_PATHNAME', 'pglogical_repl
 CREATE OR REPLACE FUNCTION pglogical.queue_truncate()
 RETURNS trigger LANGUAGE c AS 'MODULE_PATHNAME', 'pglogical_queue_truncate';
 
-CREATE OR REPLACE FUNCTION pglogical.dependency_check_trigger()
-RETURNS event_trigger LANGUAGE c AS 'MODULE_PATHNAME', 'pglogical_dependency_check_trigger';
-
 CREATE FUNCTION pglogical.pglogical_node_info(OUT node_id oid, OUT node_name text, OUT sysid text, OUT dbname text, OUT replication_sets text)
 RETURNS record
 STABLE STRICT LANGUAGE c AS 'MODULE_PATHNAME';

sql/init_fail.sql
@@ -25,7 +25,7 @@ BEGIN
 		CREATE EXTENSION IF NOT EXISTS pglogical_origin;
 	END IF;
 END;$$;
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
 ALTER EXTENSION pglogical UPDATE;
 
 -- fail (local node not existing)

commit b46575c69df85133d192b0cbef75a520bd1bcfb2

Test upgrades from 1.0
This got broken in 21cac9095 due to XL compatibility issues.

expected/init.out
@@ -36,7 +36,15 @@ BEGIN
                 CREATE EXTENSION IF NOT EXISTS pglogical_origin;
         END IF;
 END;$$;
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
+DO $$
+BEGIN
+	IF version() ~ 'Postgres-XL' THEN
+		CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
+	ELSE
+		CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+	END IF;
+END;
+$$;
 ALTER EXTENSION pglogical UPDATE;
 SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := (SELECT provider_dsn FROM pglogical_regress_variables()) || ' user=super');
  create_node 

sql/init.sql
@@ -47,7 +47,15 @@ BEGIN
         END IF;
 END;$$;
 
-CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
+DO $$
+BEGIN
+	IF version() ~ 'Postgres-XL' THEN
+		CREATE EXTENSION IF NOT EXISTS pglogical VERSION '2.0.0';
+	ELSE
+		CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0';
+	END IF;
+END;
+$$;
 ALTER EXTENSION pglogical UPDATE;
 
 SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := (SELECT provider_dsn FROM pglogical_regress_variables()) || ' user=super');