3c0f9b21602baef31d7a19436e1dd5e8801e67b4
Craig Ringer <craig@2ndquadrant.com>
2017-06-08 15:44:54 +0800

    Clean up MM tests

--- t/040_multimaster.pl
+++ t/040_multimaster.pl
@@ -14,6 +14,7 @@ use Config;
 use TestLib;
 use Test::More;
 use Data::Dumper;
+use Time::HiRes qw(gettimeofday tv_interval);
 # From Pg
 use TestLib;
 # Local
@@ -22,6 +23,7 @@ use PGLDB;
 use PGLSubscription;
 
 my $pgldb = 'pgltest';
+my $ts;
 
 #
 # We'll start with two nodes mutually replicating, but
@@ -69,7 +71,8 @@ my %common_subscribe_params = (
     replication_sets => ['default', 'set_include', 'ddl_sql'],
     forward_origins => [],
     synchronize_structure => 'true',
-    synchronize_data => 'true'
+    synchronize_data => 'true',
+    apply_delay => '500ms',
 );
 
 # subs array indexed by [from][to]
@@ -122,16 +125,44 @@ $pubs[0]->replicate_ddl(q[
     );
 ]);
 
+$pubs[0]->replicate_ddl(q[
+    CREATE TABLE public.donotreplicateme (
+        id integer primary key,
+        other integer,
+        blah text
+    );
+], ['set_exclude']);
+
 $pubs[0]->replication_set_add_table('set_include', 'tbl_included', 1);
 $pubs[0]->replication_set_add_table('set_exclude', 'tbl_excluded', 1);
+$pubs[0]->replication_set_add_table('set_exclude', 'donotreplicateme', 0);
+print "waiting for node1 to sync up after creating tables and repsets... ";
+$ts = [gettimeofday()];
 ok($subs[1][0]->wait_for_sync(), 'tables synced on 1 after add to 0');
+print " seen; took " . tv_interval ( $ts, [gettimeofday()] ) . " seconds\n";
+sleep(2);
+
+is($pubs[1]->safe_psql(q[SELECT 1 FROM information_schema.tables where table_schema = 'public' and table_name = 'tbl_included']),
+    '1', 'table tbl_included got synced');
+# The repset used for ddl was ddl_sql so the definition got replicated, even
+# though the repset we assigned for the content means the contents won't
+# replicate
+is($pubs[1]->safe_psql(q[SELECT 1 FROM information_schema.tables where table_schema = 'public' and table_name = 'tbl_excluded']),
+    '1', 'table tbl_excluded got synced');
+# wheras in this one the ddl its self didn't get replicated since we did
+# it in a nondefault repset that isn't subscribed
+is($pubs[1]->safe_psql(q[SELECT 1 FROM information_schema.tables where table_schema = 'public' and table_name = 'donotreplicateme']),
+    '', 'table donotreplicateme did NOT get synced');
 # TODO
 # Adding table membership must be done on each node??
 $pubs[1]->replication_set_add_table('set_include', 'tbl_included', 1);
 $pubs[1]->replication_set_add_table('set_exclude', 'tbl_excluded', 1);
 
 foreach my $a (0 .. 1) {
-    $pubs[$a]->poll_query_until(q[SELECT 't' FROM pglogical.local_sync_status WHERE sync_relname = 'tbl_included' AND sync_nspname = 'public' AND sync_status = 'd']);
+    print "waiting until table tbl_included enters sync state 'r' on node$a...";
+    $ts = [gettimeofday()];
+    $pubs[$a]->poll_query_until(q[SELECT 't' FROM pglogical.local_sync_status WHERE sync_relname = 'tbl_included' AND sync_nspname = 'public' AND sync_status = 'r']);
+    print " seen; took " . tv_interval ( $ts, [gettimeofday()] ) . " seconds\n";
     
     # TODO qualify by filtering for sub, or query sync status result from sub
     is($pubs[$a]->safe_psql("SELECT sync_kind, sync_status FROM pglogical.local_sync_status WHERE sync_relname = 'tbl_included' AND sync_nspname = 'public'"),
@@ -148,8 +179,15 @@ $pubs[1]->safe_psql(q[INSERT INTO tbl_included (id, blah) VALUES (1, 'from_node1
 $pubs[0]->safe_psql(q[INSERT INTO tbl_excluded (id, blah) VALUES (0, 'from_node0')]);
 $pubs[1]->safe_psql(q[INSERT INTO tbl_excluded (id, blah) VALUES (1, 'from_node1')]);
 
-$pubs[0]->poll_query_until('SELECT EXISTS (SELECT 1 FROM tbl_included WHERE other = 5)');
-$pubs[1]->poll_query_until('SELECT EXISTS (SELECT 1 FROM tbl_included WHERE other = 4)');
+print "waiting until table tbl_included gets new row blah=node1 on node0...";
+$ts = [gettimeofday()];
+$pubs[0]->poll_query_until(q[SELECT EXISTS (SELECT 1 FROM tbl_included WHERE blah = 'from_node1')]);
+print " seen; took " . tv_interval ( $ts, [gettimeofday()] ) . " seconds\n";
+
+print "waiting until table tbl_included gets new row blah=node0 on node1...";
+$ts = [gettimeofday()];
+$pubs[1]->poll_query_until(q[SELECT EXISTS (SELECT 1 FROM tbl_included WHERE blah = 'from_node0')]);
+print " seen; took " . tv_interval ( $ts, [gettimeofday()] ) . " seconds\n";
 
 my @expect = (
     [ $pubs[0], qq[0|from_node0\n1|from_node1], qq[0|from_node0] ],
