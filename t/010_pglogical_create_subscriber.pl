eb8d1c485608fa857c1e2b65ab2ca242e81257de
Pallavi Sontakke <pallavi.sontakke@2ndquadrant.com>
2018-10-24 11:20:28 +0530

    Test: Add Dbname in psql statements

--- t/010_pglogical_create_subscriber.pl
+++ t/010_pglogical_create_subscriber.pl
@@ -36,50 +36,50 @@ system("postgres -p $PROVIDER_PORT -D /tmp/tmp_datadir -c logging_collector=on &
 #allow Postgres server to startup
 system_or_bail 'sleep', '17';
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE USER super SUPERUSER";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE USER super SUPERUSER";
 
 # insert some pre-seed data
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE SEQUENCE some_local_seq";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE TABLE some_local_tbl(id serial primary key, key text unique not null, data text)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl(key, data) VALUES('key1', 'data1')";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl(key, data) VALUES('key2', NULL)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl(key, data) VALUES('key3', 'data3')";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE TABLE some_local_tbl1(id serial, key text unique not null, data text)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl1(key, data) VALUES('key1', 'data1')";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl1(key, data) VALUES('key2', NULL)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl1(key, data) VALUES('key3', 'data3')";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE TABLE some_local_tbl2(id serial, key text, data text)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl2(key, data) VALUES('key1', 'data1')";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl2(key, data) VALUES('key2', NULL)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl2(key, data) VALUES('key3', 'data3')";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE TABLE some_local_tbl3(id integer, key text, data text)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl3(key, data) VALUES('key1', 'data1')";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl3(key, data) VALUES('key2', NULL)";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "INSERT INTO some_local_tbl3(key, data) VALUES('key3', 'data3')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE SEQUENCE some_local_seq";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE TABLE some_local_tbl(id serial primary key, key text unique not null, data text)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl(key, data) VALUES('key1', 'data1')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl(key, data) VALUES('key2', NULL)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl(key, data) VALUES('key3', 'data3')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE TABLE some_local_tbl1(id serial, key text unique not null, data text)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl1(key, data) VALUES('key1', 'data1')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl1(key, data) VALUES('key2', NULL)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl1(key, data) VALUES('key3', 'data3')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE TABLE some_local_tbl2(id serial, key text, data text)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl2(key, data) VALUES('key1', 'data1')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl2(key, data) VALUES('key2', NULL)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl2(key, data) VALUES('key3', 'data3')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE TABLE some_local_tbl3(id integer, key text, data text)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl3(key, data) VALUES('key1', 'data1')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl3(key, data) VALUES('key2', NULL)";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "INSERT INTO some_local_tbl3(key, data) VALUES('key3', 'data3')";
 # Required for PostgreSQL 9.4 run
 #system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE EXTENSION IF NOT EXISTS pglogical_origin";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0'";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "ALTER EXTENSION pglogical UPDATE";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0'";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "ALTER EXTENSION pglogical UPDATE";
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := 'dbname=postgres user=super')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := 'dbname=postgres user=super')";
 
 command_ok([ 'pglogical_create_subscriber', '-D', '/tmp/tmp_backupdir', "--subscriber-name=test_subscriber", "--subscriber-dsn=$SUBSCRIBER_DSN", "--provider-dsn=$PROVIDER_DSN", '--drop-slot-if-exists', '-v', '--hba-conf=regress-pg_hba.conf', '--postgresql-conf=/tmp/tmp_datadir/postgresql.conf'], 'pglogical_create_subscriber check');
 
 #test whether preseed data is there
 
-command_ok([ 'psql', '-p', "$PGPORT", '-c', "\\d" ], 'preseed check 1 ');
-command_ok([ 'psql', '-p', "$PGPORT", '-c', "SELECT * from some_local_tbl" ], 'preseed check 2 ');
+command_ok([ 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "\\d" ], 'preseed check 1 ');
+command_ok([ 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT * from some_local_tbl" ], 'preseed check 2 ');
 
 #insert some new data
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-f', 't/basic.sql';
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-f', 't/basic.sql';
 
 #allow script to complete executing
 system_or_bail 'sleep', '11';
 
 #check whether it is replicated
-command_ok([ 'psql', '-p', "$PGPORT", '-c', "\\d" ], 'replication check 1 ');
-command_ok([ 'psql', '-p', "$PGPORT", '-c', "SELECT * from basic_dml" ], 'replication check 2 ');
-command_ok([ 'psql', '-p', "$PGPORT", '-c', "SELECT * from public.basic_dml" ], 'replication check 3 ');
+command_ok([ 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "\\d" ], 'replication check 1 ');
+command_ok([ 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT * from basic_dml" ], 'replication check 2 ');
+command_ok([ 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT * from public.basic_dml" ], 'replication check 3 ');
 
 #cleanup
 system("pg_ctl stop -D /tmp/tmp_backupdir -m immediate &");
--- t/020_non_default_replication_set.pl
+++ t/020_non_default_replication_set.pl
@@ -38,20 +38,20 @@ system("postgres -p $PGPORT -D /tmp/tmp_020_sdatadir -c logging_collector=on &")
 #allow Postgres servers to startup
 system_or_bail 'sleep', '17';
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE USER super SUPERUSER";
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "CREATE USER super SUPERUSER";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE USER super SUPERUSER";
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "CREATE USER super SUPERUSER";
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0'";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "ALTER EXTENSION pglogical UPDATE";
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "CREATE EXTENSION IF NOT EXISTS pglogical";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE EXTENSION IF NOT EXISTS pglogical VERSION '1.0.0'";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "ALTER EXTENSION pglogical UPDATE";
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "CREATE EXTENSION IF NOT EXISTS pglogical";
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := 'dbname=postgres user=super')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "SELECT * FROM pglogical.create_node(node_name := 'test_provider', dsn := 'dbname=postgres user=super')";
 
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "SELECT * FROM pglogical.create_node(node_name := 'test_subscriber', dsn := '$SUBSCRIBER_DSN')";
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT * FROM pglogical.create_node(node_name := 'test_subscriber', dsn := '$SUBSCRIBER_DSN')";
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "SELECT * FROM pglogical.create_replication_set('delay')";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "SELECT * FROM pglogical.create_replication_set('delay')";
 
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "SELECT * FROM pglogical.create_subscription(
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT * FROM pglogical.create_subscription(
     subscription_name := 'test_subscription_delay',
     provider_dsn := '$PROVIDER_DSN',
         replication_sets := '{delay}',
@@ -60,7 +60,7 @@ system_or_bail 'psql', '-p', "$PGPORT", '-c', "SELECT * FROM pglogical.create_su
         synchronize_data := false
 )";
 
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "DO \$\$
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "DO \$\$
 BEGIN
         FOR i IN 1..100 LOOP
                 IF EXISTS (SELECT 1 FROM pglogical.show_subscription_status() WHERE status = 'replicating') THEN
@@ -71,9 +71,9 @@ BEGIN
 END;
 \$\$";
 
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "SELECT subscription_name, status, provider_node, replication_sets, forward_origins FROM pglogical.show_subscription_status()";
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT subscription_name, status, provider_node, replication_sets, forward_origins FROM pglogical.show_subscription_status()";
 
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "DO \$\$
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "DO \$\$
 BEGIN
     FOR i IN 1..300 LOOP
         IF EXISTS (SELECT 1 FROM pglogical.local_sync_status WHERE sync_status = 'r') THEN
@@ -83,9 +83,9 @@ BEGIN
     END LOOP;
 END;\$\$";
 
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "SELECT sync_kind, sync_subid, sync_nspname, sync_relname, sync_status FROM pglogical.local_sync_status ORDER BY 2,3,4";
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT sync_kind, sync_subid, sync_nspname, sync_relname, sync_status FROM pglogical.local_sync_status ORDER BY 2,3,4";
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "CREATE OR REPLACE FUNCTION public.pg_xlog_wait_remote_apply(i_pos pg_lsn, i_pid integer) RETURNS VOID
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "CREATE OR REPLACE FUNCTION public.pg_xlog_wait_remote_apply(i_pos pg_lsn, i_pid integer) RETURNS VOID
 AS \$FUNC\$
 BEGIN
     WHILE EXISTS(SELECT true FROM pg_stat_get_wal_senders() s WHERE s.replay_location < i_pos AND (i_pid = 0 OR s.pid = i_pid)) LOOP
@@ -93,7 +93,7 @@ BEGIN
         END LOOP;
 END;\$FUNC\$ LANGUAGE plpgsql";
 
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "SELECT pglogical.replicate_ddl_command(\$\$
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "SELECT pglogical.replicate_ddl_command(\$\$
     CREATE TABLE public.basic_dml1 (
         id serial primary key,
         other integer,
@@ -101,8 +101,8 @@ system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "SELECT pglogical.replicate
         something interval
     );
 \$\$)";
-system_or_bail 'psql', '-p', "$PGPORT", '-c', "select * from pglogical.show_subscription_status('test_subscription_delay');";
-system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "SELECT * FROM pglogical.replication_set_add_table('delay', 'basic_dml1', true) ";
+system_or_bail 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "select * from pglogical.show_subscription_status('test_subscription_delay');";
+system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-d', "postgres", '-c', "SELECT * FROM pglogical.replication_set_add_table('delay', 'basic_dml1', true) ";
 
 #At this point subscriber sync starts crashing (`sync worker`) and recovering
 # check logs at this point at:
@@ -111,7 +111,7 @@ system_or_bail 'psql', '-p', "$PROVIDER_PORT", '-c', "SELECT * FROM pglogical.re
 # But since the table does not exist on subscriber, the sync worker dies when trying
 # to accessing it. It even logs why it dies on the line above.
 system_or_bail 'sleep', '10';
-command_fails([ 'psql', '-p', "$PGPORT", '-c', "SELECT * FROM basic_dml1" ], 'replication check');
+command_fails([ 'psql', '-p', "$PGPORT", '-d', "postgres", '-c', "SELECT * FROM basic_dml1" ], 'replication check');
 #cleanup
 system("pg_ctl stop -D /tmp/tmp_020_sdatadir -m immediate &");
 system("pg_ctl stop -D /tmp/tmp_020_pdatadir -m immediate &");
